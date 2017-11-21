a7a9168daf6b135d6ece7033695d8fd3ca6a4d03
YXCZ <minjing@mc2050.com>
2017-11-21 16:06:25 +0800

    Initial Commit

---
+++ MJToast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJToast
+//
+//  Created by YXCZ on 2017/11/21.
+//  Copyright © 2017年 JingJing_Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MJToast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MJToast
+//
+//  Created by YXCZ on 2017/11/21.
+//  Copyright © 2017年 JingJing_Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
